2344272712b195a64558525c13064a9f87acacec
Gunnar Storebo <gunnar.storebo@gmail.com>
2025-06-19 10:44:28 +0200

    Added webserver and rest API available at http://192.168.4.1/counter

--- UlrikenEncoderESP32/UlrikenEncoderESP32.ino
+++ UlrikenEncoderESP32/UlrikenEncoderESP32.ino
@@ -1,24 +1,49 @@
-// Define pins for the encoder
-#define ENCODER_CLK  2   // Connect to CLK pin
-#define ENCODER_DT   4   // Connect to DT pin
+#include <WiFi.h>
+#include <WebServer.h>
+
+// === Rotary Encoder ===
+#define ENCODER_CLK 2
+#define ENCODER_DT  4
 
 int lastClkState;
-int counter = 0;
+volatile int counter = 0;
+
+// === Wi-Fi AP credentials ===
+const char* ssid = "Ulriken";
+const char* password = "Hemmelig";
+
+// === Create web server on port 80 ===
+WebServer server(80);
+
+// === Handle GET /counter ===
+void handleCounter() {
+  String json = "{\"counter\": " + String(counter) + "}";
+  server.send(200, "application/json", json);
+}
 
 void setup() {
   Serial.begin(115200);
 
+  // Rotary encoder pin setup
   pinMode(ENCODER_CLK, INPUT_PULLUP);
   pinMode(ENCODER_DT, INPUT_PULLUP);
-
   lastClkState = digitalRead(ENCODER_CLK);
+
+  // Set up Wi-Fi AP
+  WiFi.softAP(ssid, password);
+  Serial.println("Access Point started");
+  Serial.println("IP address: " + WiFi.softAPIP().toString());
+
+  // REST API route
+  server.on("/counter", HTTP_GET, handleCounter);
+  server.begin();
 }
 
 void loop() {
+  // Rotary encoder logic
   int clkState = digitalRead(ENCODER_CLK);
   int dtState = digitalRead(ENCODER_DT);
 
-  // Detect rotation
   if (clkState != lastClkState) {
     if (dtState != clkState) {
       counter++;
@@ -30,5 +55,8 @@ void loop() {
   }
   lastClkState = clkState;
 
-  delay(1); // Small delay to reduce CPU usage
+  // Handle incoming HTTP requests
+  server.handleClient();
+
+  delay(1);
 }
